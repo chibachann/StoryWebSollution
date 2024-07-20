431db93699c9313624ae8e049413f87f791a7d34
chiba <ryotai2677@gmail.com>
2024-07-20 15:48:53 +0900

    chore: Update Footer component styles and structure

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -4,31 +4,34 @@ import * as styles from './Footer.module.css';
 const Footer = () => {
     return (
         <footer className={styles.footer}>
-            <div className={styles.Logo}>
-                <h1>Story Web Solutions</h1>
+            <div className={styles.FooterContainer}>
+                <div className={styles.Logo}>
+                    <h1>Story Web Solutions</h1>
+                </div>
+                <div className={styles.FooterMenu}>
+                    <h2>Menu</h2>
+                    <ul>
+                        <li><a href="/">Home</a></li>
+                        <li><a href="/">About</a></li>
+                        <li><a href="/">Service</a></li>
+                        <li><a href="/">Contact</a></li>
+                    </ul>
+                </div>
+                <div className={styles.SNS}>
+                    <h2>SNS</h2>
+                    <ul>
+                        <li><a 
+                            href="https://www.instagram.com/story_web_solutions?igsh=MWdzeWI0bXV3dndsdQ%3D%3D&utm_source=qr" 
+                            target="_blank" 
+                            rel="noopener noreferrer"
+                        >Instagram</a></li>
+                    </ul>
+                </div>
+                <div className={styles.CopyRight}>
+                    <p>2024 Story Web Solutions</p>
+                </div>
             </div>
-            <div className={styles.FooterMenu}>
-                <h2>Menu</h2>
-                <ul>
-                    <li><a href="/">Home</a></li>
-                    <li><a href="/">About</a></li>
-                    <li><a href="/">Service</a></li>
-                    <li><a href="/">Contact</a></li>
-                </ul>
-            </div>
-            <div className={styles.SNS}>
-                <h2>SNS</h2>
-                <ul>
-                    <li><a 
-                        href="https://www.instagram.com/story_web_solutions?igsh=MWdzeWI0bXV3dndsdQ%3D%3D&utm_source=qr" 
-                        target="_blank" 
-                        rel="noopener noreferrer"
-                    >Instagram</a></li>
-                </ul>
-            </div>
-            <div className={styles.CopyRight}>
-                <p>2024 Story Web Solutions</p>
-            </div>
+            
         </footer>
     );
 }
