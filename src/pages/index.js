88868caf1a96e17b273015875ead17a0210e252d
chiba <ryotai2677@gmail.com>
2024-07-20 13:41:20 +0900

    chore: Update site metadata and add Open Graph and Twitter tags

--- src/pages/index.js
+++ src/pages/index.js
@@ -34,11 +34,26 @@ export const Head = () => {
         }
       }
     `)
+
+    const { title, description, siteUrl, image } = data.site.siteMetadata
+
     return (
       <>
        <title>{data.site.siteMetadata.title}</title>
        <meta name="description" content={data.site.siteMetadata.description} />
-       
+       {/* Open Graph / Facebook */}
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content={siteUrl} />
+        <meta property="og:title" content={title} />
+        <meta property="og:description" content={description} />
+        <meta property="og:image" content={`${siteUrl}${image}`} />
+
+        {/* Twitter */}
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:url" content={siteUrl} />
+        <meta name="twitter:title" content={title} />
+        <meta name="twitter:description" content={description} />
+        <meta name="twitter:image" content={`${siteUrl}${image}`} />
       </>
     );
   }
