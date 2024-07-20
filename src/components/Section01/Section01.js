80ae8f951b49551adcd333bf2914b5fcf4d130e1
chiba <ryotai2677@gmail.com>
2024-07-20 09:23:11 +0900

    chore: Update animation delay in Header and adjust grid auto rows in global.css

--- src/components/Section01/Section01.js
+++ src/components/Section01/Section01.js
@@ -5,32 +5,29 @@ import * as styles from './Section01.module.css';
 const Section01 = () => {
     return ( 
         <section className={styles.section01}>
+            <div className={styles.imageArea}>
+                <img src="/images/top3.jpg" alt="Story Web Solutions" />
+            </div>
             <div className={styles.title}>
-                <h1>CAREATIVE<br/>DEGITAL</h1>
+                <h1>CAREATIVE DEGITAL</h1>
             </div>
-            <div className={styles.companyName}>
+            <div className={styles.text}>
                 <h1>Story Web Solutions</h1>
+                <p>Story Web Solutionsは、お客様のビジネスをWEBを通じてサポートします</p>
             </div>
-            <div className={styles.objectArea}>
-                
-                <div className={styles.object01}></div>
-                <div className={styles.object02}></div>
-            </div>
-            <div className={styles.backgroundImage}>
-                
-                
-                
-            </div>
-            <div className={styles.contact}>
-                <div className={styles.contactButton}>
-                    <a href="/">お問い合わせ</a>
+            <div className={styles.buttonArea}>
+                <div className={styles.contact}>
+                    <div className={styles.contactButton}>
+                        <a href="/">お問い合わせ</a>
+                    </div>
+                </div>
+                <div className={styles.catalog}>
+                    <div className={styles.catalogButton}>
+                        <a href="/">カタログを見る</a>
+                    </div>
                 </div>
             </div>
-            <div className={styles.catalog}>
-                <div className={styles.catalogButton}>
-                    <a href="/">カタログを見る</a>
-                </div>
-            </div>
+            
         </section>
     );
 };
