7ab45d5e7f4a85f42544fd5be4b946e1d3e21188
chiba <ryotai2677@gmail.com>
2024-07-20 10:54:47 +0900

    chore: Update site metadata and package description to Japanese

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
     siteMetadata: {
-        title: 'insta',
-        description: "this project is designed as a exercise to help me accustomed to using grid for layout design.",
-        siteUrl: "https://www.grid-practice02.com",
+        title: 'Story Web Solutions',
+        description: "クリエイティブなデジタルソリューションで、あなたのビジネスストーリーを紡ぐ。最先端のウェブデザインと技術で、お客様のビジョンを現実に。",
+        siteUrl: "https://www.storyweb-solutions.com",
         author: "chiba",
         lang: "ja",
     },
--- src/components/Section02/Section02.js
+++ src/components/Section02/Section02.js
@@ -11,25 +11,21 @@ const Section02 = () => {
 
     return ( 
         <section className={`${styles.section02} ${isContainerVisible ? styles.animate : ""}`} ref={containerRef}>
-            <div className={styles.ImageCardArea}>
-                <div className={styles.ImageCard}>
+            <div className={styles.ImageArea}>
 
-                </div>
-                <div className={styles.ImageCard}>
+                <img src="/images/about.jpg" alt="section02-image01" />
 
-                </div>
-                
-                
             </div>
             <div className={styles.TextArea}>
                 <div className={styles.SubTitle}>
                     <h2>About Us</h2>
                 </div>
                 <div className={styles.Title}>
-                    <h1>ストーリーを紡ぐ<br/>Webソリューション</h1>
+                    <h1>ビジネスを成功に導く<br/>Webソリューション</h1>
                 </div>
                 <div className={styles.Text}>
-                    <p>Story Web Solutionsは、使いやすく分かりやすいウェブサイトとウェブサービスを制作する会社です。私たちは、ユーザーにとって直感的で理解しやすいウェブ体験を提供することに特化しています。
+                    <p>Story Web Solutionsは、使いやすく分かりやすいウェブサイトとウェブサービスを制作する会社です。
+                        私たちは、ユーザーにとって魅力的で理解しやすいWebページを提供しています。
                     </p>
                 </div>
             </div>
